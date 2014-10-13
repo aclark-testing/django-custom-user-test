424ea6b11dbda7e8b92a3b1e9ac9e3f0716f0a12
Alex Clark <aclark@aclark.net>
2014-10-13 10:15:05 -0400

    Update

--- custom_user/custom_user/custom_user/views.py
+++ custom_user/custom_user/custom_user/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -8,10 +10,19 @@ from django.shortcuts import render
 def home(request):
     """
     """
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            # Return a 'disabled account' error message
+    else:
+        # Return an 'invalid login' error message.
     return render(request, 'home.html')
 
 
 def login(request):
-    """
-    """
     return render(request, 'signin.html')
