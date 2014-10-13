8d2d657ac410e5761bcb98d6a1734f6eefe7a838
Alex Clark <aclark@aclark.net>
2014-10-13 09:44:18 -0400

    Update

--- custom_user/custom_user/urls.py
+++ custom_user/custom_user/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'custom_user.views.home', name='home'),
+
+    url(r'^$', 'custom_user.custom_user.views.home', name='home'),
+
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
